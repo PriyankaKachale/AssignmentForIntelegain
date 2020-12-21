f45ec70b37e73bdca4c2ad5189696a70b0d31898
Priyanka Kachale <priyankakachale15@gmail.com>
2020-12-21 17:41:37 +0530

    new changes

--- assignintelgain/src/config/config.ts
+++
@@ -1,3 +0,0 @@
-export const URLS = {
-    localUrl: 'http://localhost:4001/'
-  };
--- assignintelgain/src/modules/signup.ts
+++
@@ -1,8 +0,0 @@
-export class Signup {
-  first_name: string;
-  mobile: string;
-  email: string;
-  address: string;
-  designation: string;
-  role: string;
-}
